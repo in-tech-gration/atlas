47030a46748c32060ea72bd5690413a71884fe81
Kostas Minaidis <minaidisk@gmail.com>
2026-01-04 15:18:29 +0200

    Updated youTube plugin and dependency.

--- plugins/tools/youtube/youtube.js
+++ plugins/tools/youtube/youtube.js
@@ -2,11 +2,25 @@ import { YoutubeLoader } from "@langchain/community/document_loaders/web/youtube
 import { Innertube } from "youtubei.js";
 import chalk from "chalk";
 
+function getYouTubeVideoIdFromURL(url) {
+
+  const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const match = url.match(regExp);
+
+  if (match && match[2].length == 11) {
+    return match[2];
+  } else {
+    return false;
+  }
+
+}
+
 export default async function YouTube({ options, instance }) {
+
+  const url = options.youtube;
 
   try {
 
-    const url = options.youtube
     // https://js.langchain.com/docs/integrations/document_loaders/web_loaders/youtube/
     const loader = YoutubeLoader.createFromUrl(url, {
       language: "en",
@@ -22,6 +36,19 @@ export default async function YouTube({ options, instance }) {
     console.log(docs[0].pageContent);
 
   } catch (error) {
+
+    // Try the auto-generated subtitles if all else fails:
+    disabled: {
+      break disabled;
+      const videoId = getYouTubeVideoIdFromURL(url);
+      const innertube = await Innertube.create(/* options */);
+      const videoInfo = await innertube.getInfo(videoId);
+      const captions = videoInfo.captions?.caption_tracks ?? [];
+      if (captions.length > 0 && captions[0].kind === "asr") {
+        console.log(captions[0].name); // { text: "English (auto-generated)" }
+        console.log(captions[0].base_url);
+      }
+    }
 
     console.log(chalk.redBright("Error:", error.message));
 
