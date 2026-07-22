28fb68abe7b967a1873a7c41509e2930e33c2583
Kostas Minaidis <minaidisk@gmail.com>
2026-07-23 01:29:10 +0300

    Add SRT verification plugin

---
+++ plugins/subtitles/verify-srt.plugin.js
@@ -0,0 +1,223 @@
+import fs from 'node:fs/promises';
+
+// BASED ON: https://github.com/taoning2014/srt-validator
+
+const ErrorCode = {
+  PARSER_ERROR_MISSING_TEXT: 'parserErrorMissingText',
+  PARSER_ERROR_MISSING_SEQUENCE_NUMBER: 'parserErrorMissingSequenceNumber',
+  PARSER_ERROR_INVALID_SEQUENCE_NUMBER: 'parserErrorInvalidSequenceNumber',
+  PARSER_ERROR_MISSING_TIME_SPAN: 'parserErrorMissingTimeSpan',
+  PARSER_ERROR_INVALID_TIME_SPAN: 'parserErrorInvalidTimeSpan',
+  PARSER_ERROR_INVALID_TIME_STAMP: 'parserErrorInvalidTimeStamp',
+  VALIDATOR_ERROR_START_TIME: 'validatorErrorStartTime',
+  VALIDATOR_ERROR_END_TIME: 'validatorErrorEndTime',
+  VALIDATOR_ERROR_SEQUENCE_NUMBER_START: 'validatorErrorSequenceNumberStart',
+  VALIDATOR_ERROR_SEQUENCE_NUMBER_INCREMENT: 'validatorErrorSequenceNumberIncrement',
+}
+
+class ParseError extends Error {
+  constructor(message, lineNumber, errorCode) {
+    super(message);
+    this.lineNumber = lineNumber + 1; // lineNumber is 0-indexed
+    this.errorCode = errorCode;
+  }
+}
+
+const toMS = {
+  hour: 36e5,
+  minute: 6e4,
+  second: 1e3,
+};
+
+const EOL = /\r?\n/;
+const TRAILING_WHITE_SPACE = /\s$/;
+const TIME_STAMP_REGEX = /^(\d{2}):(\d{2}):(\d{2}),(\d{3})$/;
+
+/**
+ * Parse a sequence number
+ * @param  {String} sequenceNumber
+ * @param  {Number} lineNumber - The line number currently being parsed
+ * @return {Number}
+ */
+function parseSequenceNumber(sequenceNumber, lineNumber) {
+
+  if (!sequenceNumber) {
+    throw new ParseError(
+      `Missing sequence number`,
+      lineNumber,
+      ErrorCode.PARSER_ERROR_MISSING_SEQUENCE_NUMBER
+    );
+  }
+
+  const sequenceNum = Number(sequenceNumber);
+
+  if (
+    !Number.isInteger(sequenceNum) ||
+    TRAILING_WHITE_SPACE.test(sequenceNumber)
+  ) {
+    throw new ParseError(
+      `Expected Integer for sequence number: ${sequenceNumber}`,
+      lineNumber,
+      ErrorCode.PARSER_ERROR_INVALID_SEQUENCE_NUMBER
+    );
+  }
+  return sequenceNum;
+}
+
+/**
+ * Parse a timestamp into an integer
+ * @example
+ * Input:
+ * "00:00:02,820"
+ * Output:
+ * 2820
+ * @param  {String} timeStamp - a timestamp from a timespan.
+ * @param  {Number} lineNumber - The line number currently being parsed
+ * @return {Number}
+ */
+function parseTimeStamp(timeStamp, lineNumber) {
+
+  const match = TIME_STAMP_REGEX.exec(timeStamp);
+
+  if (!match) {
+    throw new ParseError(
+      `Invalid time stamp: ${timeStamp}`,
+      lineNumber,
+      ErrorCode.PARSER_ERROR_INVALID_TIME_STAMP
+    );
+  }
+
+  const [hours, minutes, seconds, ms] = match.slice(1).map(Number);
+
+  return (
+    hours * toMS.hour + minutes * toMS.minute + seconds * toMS.second + ms
+  );
+
+}
+
+/**
+ * Parse a timespan into integer start and end values
+ * @example
+ * Input:
+ * "00:00:02,820 --> 00:00:05,120"
+ * Output:
+ * { start: 2820, end: 5120 }
+ *
+ * @param  {String} timeSpan
+ * @param  {Number} lineNumber - The line number currently being parsed
+ * @return {Object}
+ */
+function parseTimeSpan(timeSpan, lineNumber) {
+
+  if (!timeSpan) {
+    throw new ParseError(
+      `Missing time span`,
+      lineNumber,
+      ErrorCode.PARSER_ERROR_MISSING_TIME_SPAN
+    );
+  }
+
+  const [start, end] = timeSpan.split(' --> ');
+
+  if (!start || !end || TRAILING_WHITE_SPACE.test(timeSpan)) {
+    throw new ParseError(
+      `Invalid time span: ${timeSpan}`,
+      lineNumber,
+      ErrorCode.PARSER_ERROR_INVALID_TIME_SPAN
+    );
+  }
+
+  return {
+    start: parseTimeStamp(start, lineNumber),
+    end: parseTimeStamp(end, lineNumber),
+  };
+}
+
+/**
+ * Parses a given SRT file contents
+ * @param  {String} file - Contents of an SRT file in the string format
+ * @return {Array} - A list of subtitle metadata
+ */
+function parse(file) {
+
+  const lines = file.trim().split(EOL);
+
+  const result = [];
+  let lastSequenceNumber = null;
+
+  for (let i = 0; i < lines.length; i += 1) {
+
+    const lineNumbers = { chunkStart: i, timeSpan: i, text: i, chunkEnd: i };
+
+    // First line
+    const sequenceNumber = parseSequenceNumber(lines[i], i);
+    if (sequenceNumber > 1 && lastSequenceNumber + 1 !== sequenceNumber) {
+      throw new ParseError(
+        `Invalid sequence number increment: ${sequenceNumber}`,
+        sequenceNumber,
+        ErrorCode.VALIDATOR_ERROR_SEQUENCE_NUMBER_INCREMENT
+      );
+    }
+
+    // Second line
+    i += 1;
+    lineNumbers.timeSpan = i;
+    const time = parseTimeSpan(lines[i], i);
+
+    // Text can span multiple lines, so consume all lines
+    // until the separator
+
+    i += 1;
+    lineNumbers.text = i;
+    const linesOfText = [];
+    while (lines[i] && lines[i].trim()) {
+      linesOfText.push(lines[i]);
+      i += 1;
+    }
+    const text = linesOfText.join('\n');
+    if (!text) {
+      throw new ParseError(
+        `Missing caption text`,
+        i,
+        ErrorCode.PARSER_ERROR_MISSING_TEXT
+      );
+    }
+
+    lineNumbers.chunkEnd = i - 1;
+
+    lastSequenceNumber = sequenceNumber;
+
+    result.push({
+      lineNumbers,
+      sequenceNumber,
+      time,
+      text,
+    });
+  }
+
+  return result;
+}
+
+export default async function srtVerify(options, globalOptions, cli) {
+
+  const srtFile = options[0];
+
+  if (!srtFile) {
+    return console.log("Error: missing .srt input file.");
+  }
+
+  try {
+
+    // Read the SRT file
+    const data = await fs.readFile(srtFile, 'utf8');
+
+    const parsed = parse(data);
+    console.log("All good! SRT file successfully verified.");
+
+  } catch (err) {
+
+    console.error('Error:', err);
+
+  }
+
+}
