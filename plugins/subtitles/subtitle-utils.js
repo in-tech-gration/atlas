967b3b2e33b9f896331aff5beca7658afa36a112
Kostas Minaidis <minaidisk@gmail.com>
2026-09-16 21:59:36 +0300

    Add subtitle utilities (YouTube Transcript to SRT)

---
+++ plugins/subtitles/subtitle-utils.js
@@ -0,0 +1,39 @@
+import { secondsToHHMMSS } from "../tools/converter/converter.plugin.js";
+
+export function youTubeTranscript2SRT(transcript) {
+
+  let srt = [];
+
+  transcript.forEach((t, index, array) => {
+
+    const text = t.t._;
+    const startTime = Number(t.t['$'].start).toFixed();
+    const duration = Number(t.t.$.dur).toFixed();
+    const endTime = Number(startTime) + Number(duration);
+    const startTimeInHHMMSS = secondsToHHMMSS(startTime);
+    let endTimeInHHMMSS = secondsToHHMMSS(endTime);
+    const isNotLastElement = index < array.length - 1;
+    const nextStartTime = isNotLastElement ? Number(array[index + 1].t['$'].start).toFixed() : Infinity;
+    const srtIndex = index + 1;
+
+    // FIX TIMING
+    if (isNotLastElement && (endTime > Number(nextStartTime))) {
+
+      // console.log({ endTime, nextStartTime });
+      const nextStartTimeInHHMMSS = secondsToHHMMSS(nextStartTime);
+      endTimeInHHMMSS = nextStartTimeInHHMMSS;
+
+    }
+
+    // START TIME                         --> END TIME
+    // hours:minutes:seconds,milliseconds --> hours:minutes:seconds,milliseconds
+    const srtLine = `${srtIndex}\n${startTimeInHHMMSS},0 --> ${endTimeInHHMMSS},0\n${text}`
+    srt.push(srtLine);
+
+
+  });
+
+  const srtText = srt.join("\n\n");
+  return srtText;
+
+}
